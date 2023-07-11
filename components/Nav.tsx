f407799fe94b974fa31cdbf0b0148991361777af
Khair Alanam <anaambasheer@gmail.com>
2023-07-11 19:47:02 +0300

    Add Nav component tailwind styles

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,23 +5,39 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav>
-      <section>
-        <Image alt="GPRG Logo" src={logo} width={100} height={100} />
-        <h1>GPRG</h1>
+    <nav className="flex bg-dark-blue px-10 text-white py-4 lg:px-20 lg:py-5 justify-between items-center">
+      <section className="flex justify-between items-center gap-4">
+        <Image
+          alt="GPRG Logo"
+          src={logo}
+          width={100}
+          height={100}
+          className="w-10 lg:w-11"
+        />
+        <h1 className="font-light text-[1rem] lg:font-normal lg:text-[1.25rem]">
+          GPRG
+        </h1>
       </section>
-      <ul>
+      <ul className="flex justify-between gap-4 lg:gap-6 text-[0.75rem] lg:text-[1rem]">
         <li>
-          <Link href="/">Home</Link>
+          <Link href="/" className="hover:text-slate-500 transition">
+            Home
+          </Link>
         </li>
         <li>
-          <Link href="/about">About</Link>
+          <Link href="/about" className="hover:text-slate-500 transition">
+            About
+          </Link>
         </li>
         <li>
-          <Link href="/github">GitHub</Link>
+          <Link href="/github" className="hover:text-slate-500 transition">
+            GitHub
+          </Link>
         </li>
         <li>
-          <Link href="/socials">Socials</Link>
+          <Link href="/socials" className="hover:text-slate-500 transition">
+            Socials
+          </Link>
         </li>
       </ul>
     </nav>
