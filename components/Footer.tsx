ff548b075c1cda4635f27e479bfe475babd7474c
Khair Alanam <anaambasheer@gmail.com>
2023-07-15 17:26:12 +0300

    Fix copyright symbol typo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>@copy; GPRG 2023, Licensed under MIT</footer>;
+  return <footer>&copy; GPRG 2023, Licensed under MIT</footer>;
 };
 
 export default Footer;
