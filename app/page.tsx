f7b2efc7f8393147028f33a433c30992d0d2b0dc
Khair Alanam <anaambasheer@gmail.com>
2023-07-15 17:39:17 +0300

    Add hero text

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,14 @@ export const metadata = {
 export default function Home() {
   return (
     <>
-      <Nav />
+      <section className="flex flex-col justify-center items-center text-white py-16 gap-6">
+        <h1 className="text-[3rem] line-clamp-2 max-w-[50rem] text-center leading-tight">
+          Make Awesome GitHub Profile Readmes with GPRG
+        </h1>
+        <h4 className="text-[1.25rem] font-extralight">
+          Use GPRG to make your own custom profile readmes
+        </h4>
+      </section>
     </>
   );
 }
