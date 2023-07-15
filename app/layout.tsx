7f168e3013452f6fe40f50f12b7dc0bb1ee40eb2
Khair Alanam <anaambasheer@gmail.com>
2023-07-15 17:24:15 +0300

    Add main blue bg colr

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Nav from "@/components/Nav";
 import "./globals.css";
 import { Kanit } from "next/font/google";
 
@@ -18,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${kanit.className}`}>{children}</body>
+      <body className={`${kanit.className} bg-main-blue`}>
+        <Nav />
+        {children}
+      </body>
     </html>
   );
 }
