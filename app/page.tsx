70883c8abf14df60852e448bcefe0e3a9a98b613
Khair Alanam <anaambasheer@gmail.com>
2023-07-16 18:37:43 +0300

    Modify page

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ export const metadata = {
 export default function Home() {
   return (
     <>
-      <section className="flex flex-col justify-center items-center text-white py-16 gap-6">
-        <h1 className="text-[3rem] line-clamp-2 max-w-[50rem] text-center leading-tight">
+      <section className="flex flex-col justify-center items-center text-white py-16 gap-6 px-6 md:px-12">
+        <h1 className="text-[2rem] line-clamp-3 max-w-[20rem] text-center md:max-w-[40rem] leading-tight md:text-[2.5rem]">
           Make Awesome GitHub Profile Readmes with GPRG
         </h1>
-        <h4 className="text-[1.25rem] font-extralight">
+        <h4 className="text-[1rem] text-center font-extralight">
           Use GPRG to make your own custom profile readmes
         </h4>
       </section>
