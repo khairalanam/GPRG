82ef1c5e44cdacd52cd76a8c8ed8d044fe763871
Khair Alanam <anaambasheer@gmail.com>
2023-07-16 18:44:35 +0300

    Type annotations for footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Footer = () => {
+const Footer: React.FC = (): React.JSX.Element => {
   return (
     <footer className="text-white text-xs md:text-sm">
       &copy; GPRG 2023, Licensed under MIT
