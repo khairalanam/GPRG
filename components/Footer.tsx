416ca205e26e0bc3cd258913749ba9c2bc84a950
Khair Alanam <anaambasheer@gmail.com>
2023-07-16 18:38:11 +0300

    Make footer responsive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>&copy; GPRG 2023, Licensed under MIT</footer>;
+  return (
+    <footer className="text-white text-xs md:text-sm">
+      &copy; GPRG 2023, Licensed under MIT
+    </footer>
+  );
 };
 
 export default Footer;
