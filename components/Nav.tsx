b7fe7f86e87745608116e2537de63d339915bd65
Khair Alanam <anaambasheer@gmail.com>
2023-07-16 18:44:11 +0300

    Add types for navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,22 +3,22 @@ import React from "react";
 import logo from "@public/Main-Logo.svg";
 import Link from "next/link";
 
-const Nav = () => {
+const Nav: React.FC = (): React.JSX.Element => {
   return (
-    <nav className="flex bg-dark-blue px-10 text-white py-4 lg:px-20 lg:py-5 justify-between items-center">
-      <section className="flex justify-between items-center gap-4">
+    <nav className="flex bg-dark-blue px-6 md:px-10 text-white py-4 lg:px-20 lg:py-5 justify-between items-center">
+      <section className="flex justify-between items-center gap-3">
         <Image
           alt="GPRG Logo"
           src={logo}
           width={100}
           height={100}
-          className="w-10 lg:w-11"
+          className="w-8 md:w-10 lg:w-11"
         />
-        <h1 className="font-light text-[1rem] lg:font-normal lg:text-[1.25rem]">
+        <h1 className="font-light text-[0.75rem] md:text-[1rem] lg:font-normal lg:text-[1.25rem]">
           GPRG
         </h1>
       </section>
-      <ul className="flex justify-between gap-4 lg:gap-6 text-[0.75rem] lg:text-[1rem]">
+      <ul className="flex justify-between gap-3 md:gap-4 lg:gap-6 text-[0.75rem] md:text-[1rem]">
         <li>
           <Link href="/" className="hover:text-slate-500 transition">
             Home
