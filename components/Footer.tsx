44c88b9ac776b68e37ecbadb55f4c704f1e88e3a
Khair Alanam <anaambasheer@gmail.com>
2023-07-27 21:03:09 +0530

    Align footer to the bottom of the page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = (): React.JSX.Element => {
   return (
-    <footer className="text-white text-xs md:text-sm">
+    <footer className="text-white text-xs md:text-sm absolute bottom-0 left-0">
       &copy; GPRG 2023, Licensed under MIT
     </footer>
   );
