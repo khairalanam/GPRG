f386d60a0f052f70fae42af48a76991307fd805b
Khair Alanam <anaambasheer@gmail.com>
2023-07-29 21:29:21 +0530

    Make about page responsive

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,9 +7,11 @@ export const metadata: Metadata = {
 
 export default function About() {
   return (
-    <section className="text-white px-20 py-16">
-      <h1 className="text-4xl font-semibold mb-8">About GPRG</h1>
-      <p>
+    <section className="text-white px-6 py-10 md:px-10 md:py-12 lg:px-20">
+      <h1 className="text-2xl font-semibold mb-4 md:text-3xl lg:text-4xl lg:mb-10">
+        About GPRG
+      </h1>
+      <p className="text-base font-light md:font-normal lg:text-lg">
         GPRG is an app where you can generate your own profile readme easily.
         You can include GitHub stats, social handles, and other media to make
         your very own GitHub readme.
