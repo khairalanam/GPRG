7a7872cf03c812cf6e522ac28e2d97cc7e0dac23
Khair Alanam <anaambasheer@gmail.com>
2023-07-30 18:35:51 +0530

    Add favicon

--- app/socials/page.tsx
+++ app/socials/page.tsx
@@ -4,6 +4,9 @@ import Link from "next/link";
 export const metadata: Metadata = {
   title: "GPRG | Socials",
   description: "Social Handles on GPRG",
+  icons: {
+    icon: "/Main-Logo.svg",
+  },
 };
 
 export default function Socials(): React.JSX.Element {
