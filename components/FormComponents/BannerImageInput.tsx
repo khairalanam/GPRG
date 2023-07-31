8d2acf44741a55686d04204c0a19fa9cdaa4993c
Khair Alanam <anaambasheer@gmail.com>
2023-07-31 11:37:08 +0530

    Code the functionality of the banner image input and responsive

--- components/FormComponents/BannerImageInput.tsx
+++ components/FormComponents/BannerImageInput.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const BannerImageInput: React.FC = (): React.ReactElement => {
   const [bannerImage, setBannerImage] = useState<string>("");
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleBannerImageChange = (
     event: React.ChangeEvent<HTMLInputElement>
@@ -18,19 +21,34 @@ const BannerImageInput: React.FC = (): React.ReactElement => {
     }
   };
 
+  const handleBrowseClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
   return (
     <>
-      <label htmlFor="bannerImage">Banner Image:</label>
+      <label htmlFor="bannerImage" className="md:text-lg lg:text-xl">
+        Banner Image:
+      </label>
       <input
+        ref={fileInputRef}
         type="file"
         id="bannerImage"
         accept="image/*"
         onChange={handleBannerImageChange}
+        className="hidden"
       />
-      <button onClick={() => document.getElementById("bannerImage")?.click()}>
-        Browse
-      </button>
-      <p>Or drag and drop an image here</p>
+      <section
+        onClick={handleBrowseClick}
+        className="flex gap-4 items-middle justify-center border-[1px] border-red-500 mt-2 w-full px-6 py-10 bg-dark-blue rounded-lg font-extralight max-w-2xl md:py-20 md:text-lg lg:py-40 lg:text-xl"
+      >
+        <button className="bg-white text-main-blue rounded-md px-2 font-light border-[0.0625rem] transition hover:bg-main-blue hover:text-white hover:border-white md:px-4 lg:py-1 active:bg-dark-blue">
+          Browse
+        </button>
+        <p>or drag and drop an image here</p>
+      </section>
       {bannerImage && <Image src={bannerImage} alt="Banner" />}
     </>
   );
