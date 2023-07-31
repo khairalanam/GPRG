c11e1cc40106b70d60437256c640d95ebb7ae87f
Khair Alanam <anaambasheer@gmail.com>
2023-07-31 11:39:05 +0530

    Add Banner Image Input component

--- components/MainForm.tsx
+++ components/MainForm.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import BannerImageInput from "./FormComponents/BannerImageInput";
 
 const MainForm = () => {
-  return <form action=""></form>;
+  return (
+    <form className="text-white border-[2px] border-red-500 px-6 py-10 md:px-10 md:py-12 lg:px-20">
+      <h1 className="text-2xl font-semibold mb-4 md:text-3xl lg:text-4xl lg:mb-10">
+        Fill in the details
+      </h1>
+      <BannerImageInput />
+    </form>
+  );
 };
 
 export default MainForm;
