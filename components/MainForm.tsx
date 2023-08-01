588528b0bc655a71234c66d27db353396067e3ea
Khair Alanam <anaambasheer@gmail.com>
2023-08-01 12:51:28 +0530

    Include Name Input component

--- components/MainForm.tsx
+++ components/MainForm.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import BannerImageInput from "./FormComponents/BannerImageInput";
+import NameInput from "./FormComponents/NameInput";
 
 const MainForm = () => {
   return (
-    <form className="text-white border-[2px] border-red-500 px-6 py-10 md:px-10 md:py-12 lg:px-20">
+    <form className="text-white px-6 py-10 md:px-10 md:py-12 lg:px-20">
       <h1 className="text-2xl font-semibold mb-4 md:text-3xl lg:text-4xl lg:mb-10">
         Fill in the details
       </h1>
       <BannerImageInput />
+      <NameInput />
     </form>
   );
 };
