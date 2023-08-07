c6d74ef52bd71696857b48cb4a8e61d8d204a32f
Khair Alanam <anaambasheer@gmail.com>
2023-08-07 11:46:18 +0530

    Add field type

--- types/ReducerTypes.ts
+++ types/ReducerTypes.ts
@@ -3,6 +3,18 @@ export interface RoleState {
   value: string;
 }
 
+export interface ProjectState {
+  id: number;
+  title: string;
+  link: string;
+  desc: string;
+}
+
+export interface ProjectAction {
+  type: string;
+  payload: ProjectState;
+}
+
 export interface Action<T> {
   type: string;
   payload: {
@@ -17,3 +29,5 @@ export interface BioState {
   id: string;
   value: string;
 }
+
+export type Field = "title" | "link" | "desc";
