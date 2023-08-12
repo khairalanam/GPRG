dc29362a616816b786ab8ff24e590fd9971424f1
Khair Alanam <anaambasheer@gmail.com>
2023-08-12 20:53:57 +0530

    Refactor standalone types to a file

---
+++ types/NormalTypes.ts
@@ -0,0 +1,14 @@
+export interface TechStack {
+  label: string;
+  value: string;
+  icon: React.JSX.Element;
+}
+
+export interface SocialHandle {
+  label: string;
+  icon: React.JSX.Element;
+}
+
+export interface SocialLink {
+  [key: string]: string;
+}
