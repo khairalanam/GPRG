5a5e00f037025c4f5b985c373b54da066ec4dbbc
Khair Alanam <anaambasheer@gmail.com>
2023-08-12 21:05:47 +0530

    include more social handles

--- data/socialHandles.tsx
+++ data/socialHandles.tsx
@@ -1,10 +1,39 @@
 import { SocialHandle } from "@/types/NormalTypes";
-import { SiGithub, SiLinkedin, SiTwitter } from "react-icons/si";
+import {
+  SiBehance,
+  SiCodechef,
+  SiCodeforces,
+  SiDevdotto,
+  SiDribbble,
+  SiFacebook,
+  SiGithub,
+  SiHackerrank,
+  SiHashnode,
+  SiInstagram,
+  SiLeetcode,
+  SiLinkedin,
+  SiMedium,
+  SiTwitter,
+} from "react-icons/si";
 
 const socialOptions: SocialHandle[] = [
+  { label: "Facebook", icon: <SiFacebook /> },
   { label: "Twitter", icon: <SiTwitter /> },
   { label: "LinkedIn", icon: <SiLinkedin /> },
   { label: "GitHub", icon: <SiGithub /> },
+  { label: "Instagram", icon: <SiInstagram /> },
+
+  { label: "Dribbble", icon: <SiDribbble /> },
+  { label: "Behance", icon: <SiBehance /> },
+
+  { label: "Leetcode", icon: <SiLeetcode /> },
+  { label: "Codechef", icon: <SiCodechef /> },
+  { label: "Codeforces", icon: <SiCodeforces /> },
+  { label: "HackerRank", icon: <SiHackerrank /> },
+
+  { label: "Medium", icon: <SiMedium /> },
+  { label: "Devto", icon: <SiDevdotto /> },
+  { label: "Hashnode", icon: <SiHashnode /> },
 ];
 
 export default socialOptions;
