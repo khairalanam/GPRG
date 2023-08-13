47f2ab4d6f97c9126b4bf5af7d8ec7d85dc82de5
Khair Alanam <anaambasheer@gmail.com>
2023-08-13 21:22:50 +0530

    Add data for industry tag component

---
+++ data/industryTags.ts
@@ -0,0 +1,16 @@
+const industryTags: string[] = [
+  "Frontend Development",
+  "UI/UX Design",
+  "Database Management",
+  "Backend Development",
+  "Python Development",
+  "DevOps",
+  "Cloud Computing",
+  "Blockchain Technology",
+  "Artificial Intelligence",
+  "Machine Learning",
+  "Deep Learning",
+  "Cyber Security",
+];
+
+export default industryTags;
