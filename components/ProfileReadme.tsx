6cfc14f065f1f7a107f856470c07e774dbf01c7a
Khair Alanam <anaambasheer@gmail.com>
2023-08-30 21:50:42 +0530

    Add name markdown

--- components/ProfileReadme.tsx
+++ components/ProfileReadme.tsx
@@ -2,11 +2,13 @@
 import React from "react";
 import BannerImageMarkdown from "./MarkdownComponents/BannerImageMarkdown";
 import { FormData } from "@/types/NormalTypes";
+import NameMarkdown from "./MarkdownComponents/NameMarkdown";
 
 const ProfileReadme: React.FC<{ formData: FormData }> = ({ formData }) => {
   return (
     <section>
       <BannerImageMarkdown bannerImage={formData.bannerImage} />
+      <NameMarkdown name={formData.name} />
     </section>
   );
 };
