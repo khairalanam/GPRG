1fa8e2637df74136e0d99f6d27f9e5a319330f9e
Swadhin <nayak.swadhin25@gmail.com>
2023-10-02 18:39:46 +0530

    Added Blur effect to the Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 export default function Home(): JSX.Element {
   return (
     <>
-      <section className="flex flex-col justify-center items-center text-white py-16 gap-6 px-6 md:px-12">
+      <section className="flex flex-col justify-center items-center text-white py-36 gap-6 px-6 md:px-12">
         <h1 className="text-[2rem] line-clamp-3 max-w-[20rem] text-center md:max-w-[40rem] leading-tight md:text-[2.5rem]">
           Make Awesome GitHub Profile Readmes with GPRG
         </h1>
